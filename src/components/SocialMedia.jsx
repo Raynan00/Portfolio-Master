3a3ad0b0da539fd5737a5f92dd8c687e5015b06c
Raynan <wuyepraynan@gmail.com>
2022-08-17 13:51:46 +0100

    third commit

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,13 +5,13 @@ import { FaLinkedin } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://twitter.com/WuyepRaynan" target="_blank" rel="noreferrer"> <BsTwitter /></a>
     </div>
     <div>
-      <BsGithub />
+      <a href="https://github.com/Raynan00" target="_blank" rel="noreferrer"> <BsGithub /></a>
     </div>
     <div>
-      <FaLinkedin />
+      <a href="https://www.linkedin.com/in/raynan-wuyep/" target="_blank" rel="noreferrer"> <FaLinkedin /></a>
     </div>
   </div>
 );
